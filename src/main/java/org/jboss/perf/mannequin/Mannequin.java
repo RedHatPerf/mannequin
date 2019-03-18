5f24c84e4a525245f51f8dffcab587966156d0c0
Radim Vansa <rvansa@redhat.com>
2019-03-18 14:55:00 +0100

    Add proxy mode

--- src/main/java/org/jboss/perf/mannequin/Mannequin.java
+++ src/main/java/org/jboss/perf/mannequin/Mannequin.java
@@ -1,17 +1,29 @@
 package org.jboss.perf.mannequin;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.LongAdder;
 
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class Mannequin extends AbstractVerticle {
@@ -19,9 +31,12 @@ public class Mannequin extends AbstractVerticle {
 
    private static final int PORT = Integer.getInteger("mannequin.port", 8080);
    private static final String NAME;
+   public static final String X_PROXIED_BY = "x-proxied-by";
 
    private static LongAdder inflight = new LongAdder();
    private static BusyThreads busyThreads = new BusyThreads();
+
+   private WebClient client;
 
    static {
       String name = System.getenv("NAME");
@@ -30,56 +45,19 @@ public class Mannequin extends AbstractVerticle {
 
    @Override
    public void start(Future<Void> startFuture) {
+      client = WebClient.create(vertx, new WebClientOptions().setFollowRedirects(false));
+
       Router router = Router.router(vertx);
-      router.route(HttpMethod.GET, "/").handler(ctx -> {
-         HttpServerResponse response = ctx.response();
-         response.putHeader(HttpHeaderNames.SERVER, "Vert.x");
-         response.setStatusCode(HttpResponseStatus.NO_CONTENT.code()).end();
-      });
-      router.route(HttpMethod.POST, "/").handler(BodyHandler.create()).handler(ctx -> {
-         HttpServerResponse response = ctx.response();
-         response.setStatusCode(HttpResponseStatus.OK.code()).end(ctx.getBody());
-      });
+      router.route(HttpMethod.GET, "/").handler(this::handleRootGet);
+      router.route(HttpMethod.POST, "/").handler(BodyHandler.create()).handler(this::handleRootPost);
       router.route(HttpMethod.GET, "/name").handler(ctx -> ctx.response().end(NAME + "\n"));
       // Adjust worker pool size using -Dvertx.options.workerPoolSize=xxx
-      router.route(HttpMethod.GET, "/mersenneprime").handler(ctx -> {
-         String pStr = ctx.request().getParam("p");
-         int p;
-         try {
-            p = Integer.parseInt(pStr);
-            inflight.increment();
-            vertx.executeBlocking(future -> future.complete(Computation.isMersennePrime(p)), false, result -> {
-               inflight.decrement();
-               if (ctx.response().ended()) {
-                  // connection has been closed before we calculated the result
-                  return;
-               }
-               if (result.succeeded()) {
-                  ctx.response().end(String.valueOf(result.result()));
-               } else {
-                  ctx.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).end();
-               }
-            });
-         } catch (NumberFormatException e) {
-            ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
-         }
-      });
-      router.route(HttpMethod.GET, "/inflight").handler(ctx -> {
-         ctx.response().end(inflight.longValue() + "\n");
-      });
-      router.route(HttpMethod.GET, "/busy/:busy").handler(ctx -> {
-         try {
-            int busy = Integer.valueOf(ctx.pathParam("busy"));
-            if (busy < 0) {
-               ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
-               return;
-            }
-            busyThreads.set(busy);
-            ctx.response().setStatusCode(HttpResponseStatus.OK.code()).end();
-         } catch (NumberFormatException e) {
-            ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end("Cannot parse: " + ctx.pathParam("busy"));
-         }
-      });
+      router.route(HttpMethod.GET, "/mersenneprime").handler(this::handleMersennePrime);
+      router.route(HttpMethod.GET, "/inflight").handler(ctx -> ctx.response().end(inflight.longValue() + "\n"));
+      router.route(HttpMethod.GET, "/busy/:busy").handler(this::handleBusy);
+      router.route(HttpMethod.GET, "/proxy").handler(this::handleProxy);
+      router.route(HttpMethod.POST, "/proxy").handler(BodyHandler.create()).handler(this::handleProxy);
+      router.route(HttpMethod.PUT, "/proxy").handler(BodyHandler.create()).handler(this::handleProxy);
 
       vertx.createHttpServer().requestHandler(router::handle).listen(PORT, result -> {
          if (result.failed()) {
@@ -90,5 +68,104 @@ public class Mannequin extends AbstractVerticle {
             System.out.printf("Mannequin listening on port %d%n", server.actualPort());
          }
       });
+   }
+
+   private void handleRootGet(RoutingContext ctx) {
+      HttpServerResponse response = ctx.response();
+      response.putHeader(HttpHeaderNames.SERVER, "Vert.x");
+      response.setStatusCode(HttpResponseStatus.NO_CONTENT.code()).end();
+   }
+
+   private void handleRootPost(RoutingContext ctx) {
+      HttpServerResponse response = ctx.response();
+      response.setStatusCode(HttpResponseStatus.OK.code()).end(ctx.getBody());
+   }
+
+   private void handleMersennePrime(RoutingContext ctx) {
+      String pStr = ctx.request().getParam("p");
+      int p;
+      try {
+         p = Integer.parseInt(pStr);
+         inflight.increment();
+         vertx.executeBlocking(future -> future.complete(Computation.isMersennePrime(p)), false, result -> {
+            inflight.decrement();
+            if (ctx.response().ended()) {
+               // connection has been closed before we calculated the result
+               return;
+            }
+            if (result.succeeded()) {
+               ctx.response().end(String.valueOf(result.result()));
+            } else {
+               ctx.response().setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()).end();
+            }
+         });
+      } catch (NumberFormatException e) {
+         ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
+      }
+   }
+
+   private void handleBusy(RoutingContext ctx) {
+      try {
+         int busy = Integer.valueOf(ctx.pathParam("busy"));
+         if (busy < 0) {
+            ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end();
+            return;
+         }
+         busyThreads.set(busy);
+         ctx.response().setStatusCode(HttpResponseStatus.OK.code()).end();
+      } catch (NumberFormatException e) {
+         ctx.response().setStatusCode(HttpResponseStatus.BAD_REQUEST.code()).end("Cannot parse: " + ctx.pathParam("busy"));
+      }
+   }
+
+   private void handleProxy(RoutingContext ctx) {
+      List<String> urls = ctx.queryParam("url");
+      URL url = validateUrl(ctx, urls);
+      if (url == null) return;
+      HttpRequest<Buffer> request = client.request(ctx.request().method(), url.getPort() < 0 ? url.getDefaultPort() :url.getPort(), url.getHost(), urls.get(0));
+      copyRequestHeaders(ctx.request().headers(), request.headers());
+      request.sendBuffer(ctx.getBody(), result -> handleReply(result, ctx.response()));
+   }
+
+   private void handleReply(AsyncResult<HttpResponse<Buffer>> result, HttpServerResponse myResponse) {
+      if (result.succeeded()) {
+         for (Map.Entry<String, String> header : result.result().headers()) {
+            myResponse.headers().add(header.getKey(), header.getValue());
+         }
+         myResponse.headers().add(X_PROXIED_BY, NAME);
+         myResponse
+               .setStatusCode(result.result().statusCode())
+               .setStatusMessage(result.result().statusMessage())
+               .end(result.result().body());
+      } else {
+         myResponse.setStatusCode(HttpResponseStatus.INTERNAL_SERVER_ERROR.code())
+               .end(result.cause().toString());
+      }
+   }
+
+   private void copyRequestHeaders(MultiMap serverHeaders, MultiMap clientHeaders) {
+      for (Map.Entry<String, String> header : serverHeaders) {
+         if (!"host".equalsIgnoreCase(header.getKey())) {
+            clientHeaders.add(header.getKey(), header.getValue());
+         }
+      }
+   }
+
+   private URL validateUrl(RoutingContext ctx, List<String> urls) {
+      if (urls.isEmpty() || urls.size() > 1) {
+         ctx.response()
+               .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+               .setStatusMessage("Single URL required.").end();
+      }
+      URL url;
+      try {
+         url = new URL(urls.get(0));
+      } catch (MalformedURLException e) {
+         ctx.response()
+               .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+               .setStatusMessage("Invalid URL").end();
+         return null;
+      }
+      return url;
    }
 }
