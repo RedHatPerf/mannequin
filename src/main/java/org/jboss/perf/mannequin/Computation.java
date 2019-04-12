f2c690a5e618cfaf80185ac78474cb22b8b62c87
Will Reichert <willr3@gmail.com>
2019-04-12 10:31:34 -0500

    change isMersennePrime to return Json

--- src/main/java/org/jboss/perf/mannequin/Computation.java
+++ src/main/java/org/jboss/perf/mannequin/Computation.java
@@ -1,5 +1,8 @@
 package org.jboss.perf.mannequin;
 
+
+
+import javax.json.*;
 import java.math.BigInteger;
 
 public class Computation {
@@ -8,12 +11,22 @@ public class Computation {
    private static final BigInteger MINUS_TWO = BigInteger.valueOf(-2);
 
    // See https://en.wikipedia.org/wiki/Lucas%E2%80%93Lehmer_primality_test
-   public static boolean isMersennePrime(int p) {
+   public static JsonObject isMersennePrime(int p) {
+      JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+      JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+      objectBuilder.add("input",p);
+      //to ensure responses cannot be memoized
+      objectBuilder.add("startMillis",System.currentTimeMillis());
       BigInteger s = FOUR;
+      arrayBuilder.add(s);
       BigInteger M = BigInteger.valueOf(2).pow(p).add(MINUS_ONE);
       for (int i = 0; i < p - 2; ++i) {
          s = s.multiply(s).add(MINUS_TWO).mod(M);
+         arrayBuilder.add(s);
       }
-      return s.compareTo(BigInteger.ZERO) == 0;
+      objectBuilder.add("stopMillis",System.currentTimeMillis());
+      objectBuilder.add("steps",arrayBuilder.build());
+      objectBuilder.add("result",s.compareTo(BigInteger.ZERO) == 0);
+      return objectBuilder.build();
    }
 }
